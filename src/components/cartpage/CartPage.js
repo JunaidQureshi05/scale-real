d51dd24448812eceefccbf522381329e8da05107
Junaid Qureshi <Junaid.562000@gmail.com>
2022-04-25 18:55:55 +0530

    done

--- src/components/cartpage/CartPage.js
+++ src/components/cartpage/CartPage.js
@@ -12,29 +12,40 @@ const CartPage = () => {
 
   return (
     <div className="CartPage">
-      {cartItems.map((item) => (
-        <div className="CartItem">
-          <div className="image">
-            <img src={item.image} />
+      <div className="left">
+        {cartItems.map((item) => (
+          <div className="CartItem">
+            <div className="image">
+              <img src={item.image} />
+            </div>
+            <h1>{item.name}</h1>
+            <select
+              value={item.qty}
+              onChange={(e) =>
+                dispatch(addToCart(item.product, Number(e.target.value)))
+              }
+            >
+              {[...Array(10).keys()].map((x) => (
+                <option key={x + 1} value={x + 1}>
+                  {x + 1}
+                </option>
+              ))}
+            </select>
+            <button onClick={() => removeFromCartHandler(item.product)}>
+              <i className="fas fa-trash"></i>
+            </button>
           </div>
-          <h1>{item.name}</h1>
-          <select
-            value={item.qty}
-            onChange={(e) =>
-              dispatch(addToCart(item.product, Number(e.target.value)))
-            }
-          >
-            {[...Array(10).keys()].map((x) => (
-              <option key={x + 1} value={x + 1}>
-                {x + 1}
-              </option>
-            ))}
-          </select>
-          <button onClick={() => removeFromCartHandler(item.product)}>
-            <i className="fas fa-trash"></i>
-          </button>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className="right">
+        <h2>
+          Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items
+        </h2>
+        $
+        {cartItems
+          .reduce((acc, item) => acc + item.qty * item.price, 0)
+          .toFixed(2)}
+      </div>
     </div>
   );
 };
